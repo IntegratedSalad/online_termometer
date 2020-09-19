386c5ea6d0c7e5558e88eecab9fd29cf8e685fb8
Buppo <buppotheobliterator@gmail.com>
2020-09-19 13:21:37 +0200

    Revert "File loading from SPIFFS by server (used to redirect client to pages)"
    This reverts commit 2d79d71fcb9b65a963080910d4704ab303985ea9.

--- temperature_data_gatherer/web_server.ino
+++ temperature_data_gatherer/web_server.ino
@@ -1,37 +1,9 @@
-/* Functions regarding server behavior and handling data send to server */
-
-String get_content_type(String filename)
+void handle_root()
 {
-  if      (filename.endsWith(".html"))   return "text/html";
-  else if (filename.endsWith(".css"))    return "text/css";
-  else if (filename.endsWith(".js"))     return "application/javascript";
-  else if (filename.endsWith(".ico"))    return "image/x-icon";
-  else if (filename.endsWith(".json"))   return "application/json";
-  else if (filename.endsWith(".png"))    return "image/png";
-  else if (filename.endsWith(".gz"))     return "application/x-gzip";
-  return "text/plain";
+  server.send(200, "text/plain", ":DD XDD");
 }
 
-bool handle_file_read(String path)
+void handle_not_found()
 {
-  Serial.println("handle_file_read: " + path);
-  if (path.endsWith("/")) path += "index.html";
-  
-  String content_type = get_content_type(path);
-  String path_with_gz = path + ".gz";
-
-  if (LittleFS.exists(path) || LittleFS.exists(path_with_gz))
-  {
-    if (LittleFS.exists(path_with_gz))
-      path += ".gz";
- 
-    File f = LittleFS.open(path, "r");
-    size_t sent = server.streamFile(f, content_type);
-    f.close();
-    Serial.println(String("\tSent file: ") + path);
-    return true;
-  }
-  Serial.println(String("\tFile Not Found: ") + path);
-  return false;
-
+  server.send(404, "text/plain", "404: Not Found");
 }
